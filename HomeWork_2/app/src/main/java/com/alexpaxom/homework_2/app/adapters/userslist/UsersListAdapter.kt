7b560f71596950dfdf3d99a0bab15bd6e3d02310
Pakhomov Alexandr <WorkAlexAn@gmail.com>
2021-10-30 18:54:17 +0300

    fixed diff util for Users List

--- HomeWork_2/app/src/main/java/com/alexpaxom/homework_2/app/adapters/userslist/UsersListAdapter.kt
+++ HomeWork_2/app/src/main/java/com/alexpaxom/homework_2/app/adapters/userslist/UsersListAdapter.kt
@@ -1,8 +1,26 @@
 package com.alexpaxom.homework_2.app.adapters.userslist
 
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import com.alexpaxom.homework_2.app.adapters.BaseElements.BaseDiffUtilAdapter
 import com.alexpaxom.homework_2.data.models.User
 
 class UsersListAdapter(
     usersListFactoryHolders: UsersListFactoryHolders
 ): BaseDiffUtilAdapter<User>(usersListFactoryHolders)
+{
+    init {
+        diffUtil = AsyncListDiffer(this, UsersListAdapterDiffUtil())
+    }
+
+    class UsersListAdapterDiffUtil: DiffUtil.ItemCallback<User>(){
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean
+           = oldItem.id == newItem.id
+
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean
+            = oldItem == newItem
+
+    }
+
+}
--- HomeWork_2/app/src/main/java/com/alexpaxom/homework_2/data/models/User.kt
+++ HomeWork_2/app/src/main/java/com/alexpaxom/homework_2/data/models/User.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User(
+data class User(
     override val typeId: Int = 0,
     val id: Int,
     val name: String,
